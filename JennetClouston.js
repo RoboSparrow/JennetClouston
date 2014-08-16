c1ebb0752eb265dd511b13da07b794827dc7a849
Joerg Boeselt <net@boeselt.net>
2014-08-16 19:29:24 +0930

    Hoot, hoot! First commit

---
+++ JennetClouston.js
@@ -0,0 +1,61 @@
+/**
+ * defaults
+ */
+var defaults = ['Shaws', ' the laird'];
+var curse = '"That is the house of {0}!" she cried. "Blood built it; blood stopped the building of it; blood shall bring it down. See here!" she cried again - "I spit upon the ground, and crack my thumb at it! Black be its fall! If ye see {1}, tell him what ye hear; tell him this makes the twelve hunner and nineteen time that Jennet Clouston has called down the curse on him and his house, byre and stable, man, guest, and master, wife, miss, or bairn - black, black be their fall!"';
+
+/**
+ * sprintf
+ */
+if (!String.format) {
+	String.format = function(format) {
+		var args = Array.prototype.slice.call(arguments, 1);
+		console.log(args);
+		return format.replace(/{(\d+)}/g, function(match, number) {
+			return typeof args[number] != 'undefined' ? args[number] : match;
+		});
+	};
+}
+
+/*^
+ * The curse
+ */
+var requestCurse = function(e) {
+
+	if (e.preventDefault) {
+		e.preventDefault();
+	}
+	
+	var wait = document.createElement('pre');
+	wait.appendChild(document.createTextNode('Yer have to wait until Jenny is ready!'));
+	document.getElementById('JennyCloustonsCourse').appendChild(wait);
+	document.getElementById('submit').disabled = true;
+	
+	var victim = [];
+
+	victim.push((this.house.value) ? this.house.value : defaults[0]);
+	victim.push((this.name.value) ? this.name.value : defaults[1]);
+	var query = String.format.apply(this, [curse].concat(victim));
+
+	var audio = new Audio();
+	audio.src = './tts.php?ie=utf-8&tl=en&q=' + query;
+	
+	audio.addEventListener("canplaythrough", function() {
+		audio.play();
+		document.getElementById('JennyCloustonsCourse').removeChild(wait); 
+	});
+	audio.addEventListener("ended", function() {
+		document.getElementById('submit').disabled = false;
+	});
+
+	return;
+};
+
+/**
+ * Init
+ */
+(function(window) {
+	document.addEventListener("DOMContentLoaded", function(event) {
+		document.getElementById('JennyCloustonsCourse').addEventListener("submit", requestCurse);
+	});
+})(window);
---
+++ readme.md
@@ -0,0 +1,31 @@
+Jennet Clouston's Curse Generator
+=================================
+
+**Jenny Clouston at your service.**
+
+HowTo
+-----
+
+Browse to `curse.html` and enter your foes's **name** and **house**.  (If you don't enter anything the defaults from the novel will be used.)
+
+About
+---------
+
+Jennet Clouston is a marginal character from Robert Lois Stevenson's novel ["Kidnapped"](http://en.wikipedia.org/wiki/Kidnapped_%28novel%29).
+The main character David Balfour encounters the *stout, dark, sour-looking woman* on his way to the House of Shaws in Southern Scotland, the castle of his anchestors.
+
+Asked for the way to the mansion, Jennet casts **one of the best curses in literature** on the House of Shaws and it's current landlord, Ebenezer Balfour.
+
+### The Curse:
+
+> "That is the house of **Shaws**!" she cried. "Blood built it; blood stopped the building of it; blood shall bring it down. See here!" she cried again -- "I spit upon the ground, and crack my thumb at it! Black be its fall! If ye see **the laird**, tell him what ye hear; tell him this makes the twelve hunner and nineteen time that Jennet Clouston has called down the curse on him and his house, byre and stable, man, guest, and master, wife, miss, or bairn -- black, black be their fall!"
+
+Taken from: [About.com - Classic Literature](http://classiclit.about.com/library/bl-etexts/rlstevenson/bl-rlst-kid-2.htm)
+
+Tecs
+----
+
+The little app uses the [Google  Translate public API](https://translate.google.com/) to give Jenny a voice. Unfortunately the  API is not CORS enabled (and  likely never will be), so a small server script is required for fetching and building the mp3 .
+
+* requires PHP
+* Browser compatibility: > IE 9
