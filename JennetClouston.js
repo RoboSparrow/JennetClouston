f079ef309b37cab06bcb55ef89102f7ddb5c25e2
Joerg Boeselt <net@boeselt.net>
2014-09-10 22:48:37 +0930

    some code formatting

--- JennetClouston.js
+++ JennetClouston.js
@@ -3,8 +3,8 @@
 /**
  * defaults
  */
-var defaults = ['Shaws', ' the laird', 'him', 'his'];
-var classes = ['house', ' name', 'sex', 'sex1'];
+var defaults = ['Shaws', 'the laird', 'him', 'his'];
+var classes = ['house', 'name', 'sex', 'sex1'];
 
 var curse = '"That is the house of {0}!" she cried. "Blood built it; blood stopped the building of it; blood shall bring it down. See here!" she cried again - "I spit upon the ground, and crack my thumb at it! Black be its fall! If ye see {1}, tell {2} what ye hear; tell {2} this makes the twelve hunner and nineteen time that Jennet Clouston has called down the curse on {2} and {3} house, byre and stable, man, guest, and master, wife, miss, or bairn - black, black be their fall!"';
 
@@ -24,36 +24,36 @@ if (!String.format) {
  * The curse
  */
 var JennetCloustonCurse  = function(){
-	
-	this.request = function(form){
-		var node = document.getElementById('JennyCloustonsCurse');
-		
+    
+    this.request = function(form){
+        var node = document.getElementById('JennyCloustonsCurse');
+        
         var wait = document.createElement('pre');
-		wait.appendChild(document.createTextNode('Yer have to wait until Jenny is ready!'));
+        wait.appendChild(document.createTextNode('Yer have to wait until Jenny is ready!'));
         node.parentNode.insertBefore(wait, node.nextSibling);
         
-		document.getElementById('submit').disabled = true;
-		
-		var victim = this.victim(form);
-		//var query = String.format.apply(this, [curse].concat(victim));
+        document.getElementById('submit').disabled = true;
+        
+        var victim = this.victim(form);
+        //var query = String.format.apply(this, [curse].concat(victim));
         var query = strip(node);
 
-		var audio = new Audio();
-		audio.src = './tts.php?ie=utf-8&tl=en&q=' + query;
-		audio.addEventListener('canplaythrough', function() {
-			audio.play();
-			node.parentNode.removeChild(wait); 
-		});
-		audio.addEventListener('ended', function() {
-			document.getElementById('submit').disabled = false;
-		});
-	};
-	
-	function strip(node){
-		return node.textContent || node.innerText || "";
-	};
-	
-	this.inject = function(values, tag){
+        var audio = new Audio();
+        audio.src = './tts.php?ie=utf-8&tl=en&q=' + query;
+        audio.addEventListener('canplaythrough', function() {
+            audio.play();
+            node.parentNode.removeChild(wait); 
+        });
+        audio.addEventListener('ended', function() {
+            document.getElementById('submit').disabled = false;
+        });
+    };
+    
+    function strip(node){
+        return node.textContent || node.innerText || "";
+    };
+    
+    this.inject = function(values, tag){
         var inject = [];
         for(var i = 0; i < values.length; i++){
             var wrap = ['', ''];
@@ -61,22 +61,22 @@ var JennetCloustonCurse  = function(){
                 wrap[0] = '<' + tag + ' class="' + classes[i] +'">';
                 wrap[1] = '</' + tag + '>';
             }
-			inject.push(wrap[0] + values[i] + wrap[1]);
-		}
+            inject.push(wrap[0] + values[i] + wrap[1]);
+        }
         return String.format.apply(this, [curse].concat(inject));
-	};
-	
-	this.victim = function(form){
-		var victim = [];
-		victim.push((form.house.value) ? form.house.value : defaults[0]);
-		victim.push((form.name.value) ? form.name.value : defaults[1]);
-		victim.push((form.sex.value) ? form.sex.value : defaults[2]);
+    };
+    
+    this.victim = function(form){
+        var victim = [];
+        victim.push((form.house.value) ? form.house.value : defaults[0]);
+        victim.push((form.name.value) ? form.name.value : defaults[1]);
+        victim.push((form.sex.value) ? form.sex.value : defaults[2]);
         victim.push((form.sex.value) ? form.sex.value : defaults[3]);
-		return victim;
-	}
-	
-	this.update = function(element){
-		
+        return victim;
+    }
+    
+    this.update = function(element){
+        
         var value = element.value;
         if(element.name == 'sex'){
             if(value == 'lass'){
@@ -88,7 +88,7 @@ var JennetCloustonCurse  = function(){
             }
         }
         updateCurse('.' + element.name, value);
-	};
+    };
     
     function updateCurse(selector, value){
         var nodes = document.querySelectorAll(selector);
